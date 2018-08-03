0fd0366398de884eb8dbeac2868e3eea52db03ab
M. Olcay TERCANLI <m.olcay25@gmail.com>
2018-08-03 11:35:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/molcay/milli-piyango",
     packages=setuptools.find_packages(),
+    install_requires=['requests'],
     classifiers=(
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
